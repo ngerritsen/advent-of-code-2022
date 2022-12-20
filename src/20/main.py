28719b259f637c0894bba93a9b234d0dbe072523
Niels Gerritsen <n.gerritsen@protonmail.com>
2022-12-20 09:28:02 +0100

    Day 20 (Part 2)

--- src/20/main.py
+++ src/20/main.py
@@ -1,55 +1,40 @@
 import os
 
+KEY = 811589153
+IDX = [1000, 2000, 3000]
+
 
 def main():
     with open(os.path.dirname(__file__) + "/input.txt") as f:
         lines = f.read().split("\n")
 
-    coords = list()
+    coords = [(i, int(line)) for i, line in enumerate(lines)]
+    decrypted = [(c[0], c[1] * KEY) for c in coords]
 
-    for i, line in enumerate(lines):
-        coords.append((i, int(line)))
-
-    un_mix_coords(coords)
-
-    print(get_coord_sum(coords))
+    print(get_coord_sum(un_mix_coords(list(coords))))
+    print(get_coord_sum(un_mix_coords(decrypted, 10)))
 
 
-def un_mix_coords(coords):
-    length = len(coords)
+def un_mix_coords(coords, times=1):
+    for _ in range(times):
+        for i in range(len(coords)):
+            curr, val = find_index_and_val(coords, i)
+            move_to = (curr + val) % (len(coords) - 1)
+            move_to = len(coords) - 1 if move_to == 0 else move_to
+            coords.insert(move_to, coords.pop(curr))
 
-    for i in range(length):
-        curr, val = find_index_and_val(coords, i)
-        move_to = (curr + val) % (length - 1)
-
-        if move_to == curr:
-            continue
-
-        move_to = length - 1 if move_to == 0 else move_to
-        coords.insert(move_to, coords.pop(curr))
+    return coords
 
 
 def get_coord_sum(coords):
-    length = len(coords)
-    zero_index = find_zero_index(coords)
-
-    x = coords[(zero_index + 1000) % length][1]
-    y = coords[(zero_index + 2000) % length][1]
-    z = coords[(zero_index + 3000) % length][1]
-
-    return x + y + z
+    zero_index = [c[1] for c in coords].index(0)
+    return sum([coords[(zero_index + m) % len(coords)][1] for m in IDX])
 
 
 def find_index_and_val(coords, oi):
     for i, item in enumerate(coords):
         if item[0] == oi:
             return i, item[1]
-
-
-def find_zero_index(coords):
-    for i, item in enumerate(coords):
-        if item[1] == 0:
-            return i
 
 
 if __name__ == '__main__':
