12af32f7615eb88abd054c50709354357d92dd04
Niels Gerritsen <n.gerritsen@protonmail.com>
2022-12-21 14:58:37 +0100

    Day 21 (Part 2)

--- src/21/main.py
+++ src/21/main.py
@@ -5,18 +5,29 @@ def main():
     with open(os.path.dirname(__file__) + "/input.txt") as f:
         lines = f.read().split("\n")
 
-    constants = dict()
-    calculations = dict()
+    constants, calculations = parse_monkeys(lines)
+    print(find_root(constants.copy(), calculations.copy()))
+    print(find_human(constants.copy(), calculations.copy()))
 
-    for line in lines:
-        parts = line.split(" ")
-        key = parts[0].strip(":")
 
-        if len(parts) == 4:
-            calculations[key] = tuple(parts[1:4])
+def find_human(constants, calculations):
+    high, low = 1e15, -1e15
+
+    while high > low:
+        mid = int((high + low) / 2)
+        c = constants.copy()
+        c["humn"] = mid
+        diff = find_root(c, calculations.copy(), True)
+
+        if diff == 0:
+            return mid
+        elif diff > 0:
+            low = mid + 1
         else:
-            constants[key] = int(parts[1])
+            high = mid - 1
 
+
+def find_root(constants, calculations, eq=False):
     while len(calculations) > 0:
         del_keys = list()
 
@@ -24,6 +35,9 @@ def main():
             if ak in constants and bk in constants:
                 a, b = constants[ak], constants[bk]
                 ans = 0
+
+                if key == "root" and eq:
+                    return a - b
 
                 if op == "+":
                     ans = a + b
@@ -40,7 +54,22 @@ def main():
         for key in del_keys:
             del calculations[key]
 
-    print(int(constants["root"]))
+    return int(constants["root"])
+
+
+def parse_monkeys(lines):
+    constants, calculations = dict(), dict()
+
+    for line in lines:
+        parts = line.split(" ")
+        key = parts[0].strip(":")
+
+        if len(parts) == 4:
+            calculations[key] = tuple(parts[1:4])
+        else:
+            constants[key] = int(parts[1])
+
+    return constants, calculations
 
 
 if __name__ == '__main__':
